64f26985cce59cfb889306f75f3df24181be5c72
Rushi <Rushikeshharihar.Kasture@cognizant.com>
2022-10-22 16:23:13 +0530

    changes in viewgoal final

--- src/charts/ViewGoals.jsx
+++ src/charts/ViewGoals.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import { CircularProgressbar } from "react-circular-progressbar";
-// import "react-circular-progressbar/dist/styles.css";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function ViewGoals() {
   const percentage = 66;
   return (
     <div style={{ width: "30%" }}>
-      {/* <CircularProgressbar value={percentage} text={`${percentage}%`} /> */}
+      <CircularProgressbar value={percentage} text={`${percentage}%`} />
     </div>
   );
 }
